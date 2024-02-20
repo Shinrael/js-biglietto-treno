90f1832f097fdb669d7b129287653f429fae007f
Shinrael <mircoelena89@gmail.com>
2024-02-20 15:40:10 +0100

    [add] another steps

--- README.md
+++ README.md
@@ -5,5 +5,6 @@ Calcolo del prezzo del biglietto del treno
 1. Attraverso un prompt chiediamo i km che vuole percorrere
 2. Attraverso un prompt chiediamo l'età del passeggero
 3. Calcolare il prezzo del biglietto in base alla tariffa attuale (0.21 € al km)
-4. A seconda delle condizioni calcolare uno sconto del 20% se minorenne o del 40 % per gli over 65
-5. Stampare il prezzo finale sull'html con un massimo di due decimali dopo la virgola.
+4. Stampo il prezzo senza condizioni nell'HTML
+5. A seconda delle condizioni calcolare uno sconto del 20% se minorenne o del 40 % per gli over 65
+6. Aggiungo la stampa del prezzo finale sull'html con un massimo di due decimali dopo la virgola.
--- js/script.js
+++ js/script.js
@@ -5,6 +5,32 @@ const kilometres = prompt ('Quanti km si desidera percorrere?');
 // 2.
 
 const age = prompt ('Quanti anni hai?');
-console.log(kilometres, age);
 
 // 3.
+
+const rate = 0.21
+const price = rate * kilometres
+console.log(kilometres, age, price);
+
+// 4.
+
+document.getElementById ('output').innerHTML += price
+
+// 5.
+
+const priceUnderage = ''
+const priceAncient = ''
+
+if (age > 18){
+  priceUnderage = ((price * 20) / 100);
+} 
+
+if (age > 65){
+  priceAncient = ((price * 40) / 100);
+}
+
+console.log(priceUnderage, priceAncient);
+
+document.getElementById ('discount').innerHTML = `
+<h2> ma hai diritto ad uno sconto, perciò il prezzo finale è di <strong> ${}
+`
